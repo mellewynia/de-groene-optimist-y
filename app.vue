0b357c31ccdf2059aebda54dac3fcb3c46b2ce1d
Melle Wynia <git@mellewynia.nl>
2023-05-12 14:38:51 +0200

    Fix title

--- app.vue
+++ app.vue
@@ -9,4 +9,12 @@
 
 <script setup lang="ts">
 import '@fontsource/open-sans/variable.css'
+
+useHead({
+  titleTemplate: (productCategory) => {
+    return productCategory
+      ? `${productCategory} - De Groene Optimist`
+      : 'De Groene Optimist'
+  }
+})
 </script>
